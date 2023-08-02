4a99d6da3003c25389be7d47a426e930bea466e5
MethRa <MethRa@METHRA>
2023-08-02 23:06:22 +0300

    Proje dosyası ekle.

---
+++ KisiselWebProjesi/Models/Siniflar/Context.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace KisiselWebProjesi.Models.Siniflar
+{
+    public class Context : DbContext
+    {
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<AnaSayfa> AnaSayfas { get; set; }
+        public DbSet<icons> icons { get; set; }
+
+    }
+}
---
+++ KisiselWebProjesi/Models/Siniflar/icons.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KisiselWebProjesi.Models.Siniflar
+{
+    public class icons
+    {
+        [Key]
+        public int id { get; set; }
+        public string icon { get; set; }
+        public string link { get; set; }
+
+    }
+}
